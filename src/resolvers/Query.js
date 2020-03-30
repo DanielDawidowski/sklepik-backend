582ddcbeca3df3de3a60d5416f8dadff40eb1095
Daniel Dawidowski <dvds1987@gmail.com>
2020-03-30 15:28:48 +0200

    Adding Pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
    events: forwardTo('db'),
    event: forwardTo('db'),
-   
+   eventsConnection: forwardTo('db'),
     // async events(parent, args, ctx, info) {
     //     const events = await ctx.db.query.events();
     //     return events;
